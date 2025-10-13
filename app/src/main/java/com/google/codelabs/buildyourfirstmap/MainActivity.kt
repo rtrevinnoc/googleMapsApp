43534f5abc0f67e69e460365cc191066ca551863
Jenifer 1974337 <pamela.carrillogrc@uanl.edu.mx>
2025-10-12 21:42:41 -0600

    Implementacion de mapa de google maps

--- app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -1,61 +1,36 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.codelabs.buildyourfirstmap
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.codelabs.buildyourfirstmap.place.Place
-import com.google.codelabs.buildyourfirstmap.place.PlacesReader
 
-class MainActivity : AppCompatActivity() {
-    private val places: List<Place> by lazy {
-        PlacesReader(this).read()
-    }
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(true)
-            .setServerClientId(baseContext.getString(R.string.default_web_client_id))
-            .build()
 
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()*/
-
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.sampleMap)  as? SupportMapFragment
-        mapFragment?.getMapAsync { map ->
-            addMarkers(map)
-            map.setInfoWindowAdapter(MarkerPopupAdapter(this))
-        }
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-    private fun addMarkers(map: GoogleMap) {
-        places.forEach { place ->
-            val marker = map.addMarker(
-                MarkerOptions()
-                    .title(place.name)
-                    .position(place.latLng)
-            )
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
-            marker?.tag = place
-        }
+
+        val monterrey = LatLng(25.6866, -100.3161)
+
+        mMap.addMarker(MarkerOptions().position(monterrey).title("Marcador en Monterrey"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(monterrey, 10f))
     }
 }
