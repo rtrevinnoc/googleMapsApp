fb793883ff713deeabc76712a40e979273deb4a9
yepono <tonbarrz5@gmail.com>
2025-10-17 23:37:08 -0600

    Implementacion de autentificacion con google

--- app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -11,34 +11,68 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package com.google.codelabs.buildyourfirstmap
+
+import com.google.firebase.auth.FirebaseAuth
+
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.codelabs.buildyourfirstmap.place.Place
 import com.google.codelabs.buildyourfirstmap.place.PlacesReader
+import com.google.firebase.Firebase
+import android.credentials.GetCredentialRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
     private val places: List<Place> by lazy {
         PlacesReader(this).read()
     }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var client = GoogleSignInClient
+
+    private lateinit var user : FirebaseUser?
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(true)
-            .setServerClientId(baseContext.getString(R.string.default_web_client_id))
+        val auth = FirebaseAuth.getInstance()
+
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(resId = R.string.default_web_client_id))
+            .requestEmail()
             .build()
 
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
+        val client = GoogleSignIN.getClient(activity = this, options)
+
+        findViewById<Button>(id = R.id.signIn).setOnClickListener {
+            signIn()
+        }
+
+
+  /*      val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(true)
+            .setServerClientId(baseContext.getString(R.string.default_web_client_id))
             .build()*/
+
+   /*     val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        if (credential is CustomCredential && credential.type == TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+            firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+        } else {
+            Log.w(TAG, "Credential is not of type Google ID!")
+        }*/
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.sampleMap)  as? SupportMapFragment
         mapFragment?.getMapAsync { map ->
@@ -58,4 +92,33 @@ class MainActivity : AppCompatActivity() {
             marker?.tag = place
         }
     }
+
+    private val signIntentHandler = registerForActivityResult(
+        contact = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val account = GoogleSignIn.getSignedInAccountFromIntent( data = result.data)
+        val account = accountIntent.getResult()
+        signInWithGoogle(tokenId = account.idToken!!)
+
+    }
+
+
+    private fun signIn(){
+        val intent = client.signInIntent
+        signInHandler.launch(input = intent)
+    }
+
+    private fun signInWithGoogle(tokenId: String) {
+        val credential = GoogleAuthProvider.getCredential(p0 = tokenId, p1 = null)
+        auth.signInWithCredential(p0 = credential)
+            .addOnCompleteListener( p0 =this){ task ->
+                if (task.isSuccesful){
+                    user = auth.currentUser
+                }else{
+                    Log.w(tag = "AUTH", msg = "FAIL")
+                }
+            }
+
+    }
+
 }
--- app/src/main/java/com/google/codelabs/buildyourfirstmap/MarkerPopupAdapter.kt
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/MarkerPopupAdapter.kt
@@ -10,16 +10,16 @@ import com.google.codelabs.buildyourfirstmap.place.Place
 
 class MarkerPopupAdapter(
     private val context: Context
-) : GoogleMap.InfoWindowAdapter {
-    override fun getInfoContents(marker: Marker): View? {
-        val place = marker.tag as? Place ?: return null
+) : GoogleMap.InfoWindowAdapter { // por defecto cuando se hace un marcador en el mapa lanza un popup personalizado
+    override fun getInfoContents(marker: Marker): View? { // es el contenido del infoWindowAdapter
+        val place = marker.tag as? Place ?: return null //
 
         val view = LayoutInflater.from(context).inflate(
             R.layout.marker_popup, null
         )
         view.findViewById<TextView>(R.id.marker_popup_title).text = place.name
         view.findViewById<TextView>(R.id.marker_popup_address).text = place.address
-        view.findViewById<TextView>(R.id.marker_popup_rating).text = "Puntuación: %.2f".format(place.rating)
+        view.findViewById<TextView>(R.id.marker_popup_rating).text = "Puntuación: %.2f".format(place.rating) //place.rating es float
 
         return view
     }
